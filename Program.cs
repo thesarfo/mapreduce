e8b5b39de8ff99f0fce901bbfc70e949e745dfcd
thesarfo <ernestsarfo100@gmail.com>
2025-10-17 09:56:09 +0000

    update program to support sequential and parallel execution modes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using mapreduce;
+﻿using mapreduce;
 using MapReduce;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("MapReduce Framework - Parallel Execution\n");
 
 var mapper = new Mapper();
 var reducer = new Reducer();
 
-var job = new MapReduceJob(mapper, reducer);
-job.ReadInput("input.txt", "output.txt");
+Console.WriteLine("Select execution mode:");
+Console.WriteLine("1. Sequential");
+Console.WriteLine("2. Parallel");
+Console.Write("\nEnter choice (1 or 2): ");
+
+var choice = Console.ReadLine();
+
+if (choice == "1")
+{
+    Console.WriteLine("\n=== SEQUENTIAL EXECUTION ===\n");
+    var job = new MapReduceJob(mapper, reducer);
+    job.ReadInput("input.txt", "output.txt");
+}
+else
+{
+    Console.WriteLine("\n=== PARALLEL EXECUTION ===\n");
+    
+    int numReducers = Environment.ProcessorCount; 
+    int chunkSizeInMB = 1;
+    
+    var parallelJob = new ParallelMapReduceJob(mapper, reducer, numReducers, chunkSizeInMB);
+    await parallelJob.ExecuteAsync("input.txt", "output.txt");
+}
+
+Console.WriteLine("\nJob completed successfully!");
